947b6384311529254c294f03842580509af6de92
Linnea Serving <linnea.serving@elev..ga.ntig.se>
2023-09-06 09:59:32 +0200

    Laggt in decimaltals-operationer

--- src/App.java
+++ src/App.java
@@ -9,18 +9,28 @@ public class App {
         int summa_subtraktion = tal1-tal2; 
 
         System.out.println("summan av tal + tal2 är "+summa_addition);
-        System.out.println("summan av tal tal2 är "+summa_subtraktion);
+        System.out.println("Differensen av tal - tal2 är "+summa_subtraktion);
 
         int produkt = tal1*tal2;
         int kvot = tal1/tal2;
 
-        System.out.println("Produkten av tal1*tal2 är"+produkt);
-        System.out.println("Kvoten av tal1/tal2 är"+kvot);
+        System.out.println("Produkten av tal1*tal2 är "+produkt);
+        System.out.println("Kvoten av tal1/tal2 är "+kvot);
 
         int rest1 = 10%3;
         int rest2 = 9%3;
 
         System.out.println("Resten av 10/3 är "+rest1);
         System.out.println("Resten av 9/3 är "+rest2);
+
+        Double decimal_tal1 = 5.0;
+        Double decimal_tal2 = 23.41;
+
+        Double dec_summa_add = decimal_tal1+decimal_tal2;
+        Double dec_summa_sub = decimal_tal1-decimal_tal2;
+
+        System.out.println("Summan av additionen är "+dec_summa_add);
+        System.out.println("Differensen av subrtactionen är "+dec_summa_sub);
+
     }
 }
